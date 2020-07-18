1d65fea7ee1e442575c0585246989783f1d3c402
SalvadorLekan <lekansalvador@gmail.com>
2020-07-18 11:11:07 +0100

    new

---
+++ precache-manifest.b6d02a0ef0d67096822fd213407f392e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff89ecd48499298997c96021b9af0448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eec680cebb4602624a3f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e95f15420c692f465d16",
+    "url": "/static/css/main.38da2850.chunk.css"
+  },
+  {
+    "revision": "eec680cebb4602624a3f",
+    "url": "/static/js/2.6c0c1513.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6c0c1513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95f15420c692f465d16",
+    "url": "/static/js/main.82bca1a2.chunk.js"
+  },
+  {
+    "revision": "60b1287e57a499df54fc",
+    "url": "/static/js/runtime-main.21ead36e.js"
+  }
+]);
